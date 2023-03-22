ef03ad1642849e9762dae91a9b94b0ee9bdc9061
sourav <souravlal61@gmail.com>
2023-03-22 19:33:28 +0530

    home

--- src/pages/homesection2.jsx
+++ src/pages/homesection2.jsx
@@ -59,9 +59,9 @@ export default function Items() {
     <div className="bg-orange-50">
       <div className="mx-auto max-w-8xl px-4 sm:px-6 lg:px-8">
         <div className="mx-auto max-w-2xl py-16 sm:py-24 lg:max-w-none lg:py-32">
-        <h2 className="text-4xl font-bold bg-orange-50"><center>Our Items</center></h2>
-
-          <div className="mt-6 space-y-12 lg:grid lg:grid-cols-3 lg:gap-x-6 lg:space-y-0">
+        <h2 className="text-4xl font-bold bg-orange-50"><center>Our Menu Items</center></h2>
+        <h2 className="text-3xl font-bold bg-orange-50"><center>Feel the joy of mouthwatering food.</center></h2>
+        <div className="mt-6 space-y-12 lg:grid lg:grid-cols-3 lg:gap-x-6 lg:space-y-0">
             {callouts.map((callout) => (
               <div key={callout.name} className="group relative">
                 <div className="relative h-80 w-full overflow-hidden rounded-lg bg-white group-hover:opacity-75 sm:aspect-w-2 sm:aspect-h-1 sm:h-64 lg:aspect-w-1 lg:aspect-h-1">
